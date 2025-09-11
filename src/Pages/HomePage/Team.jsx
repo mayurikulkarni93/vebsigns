36b9a62eee153effd066a1b702d4a2bfa4d79151
a-dudhankar <amitgit1100@gmail.com>
2025-09-11 17:01:05 +0530

    Changes

--- src/Pages/HomePage/Team.jsx
+++ src/Pages/HomePage/Team.jsx
@@ -6,32 +6,79 @@ const Team = () => {
             <section className="py-12 bg-white sm:py-16 lg:py-20">
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="text-center">
-                        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">Our Investors & Board of Directors</h2>
+                        <h2 className="text-3xl font-bold text-gray-900 sm:text-4xl xl:text-5xl font-pj">
+                            Meet Our Team
+                        </h2>
+                        <p className="mt-4 text-lg text-gray-600 max-w-2xl mx-auto font-pj">
+                            A group of passionate innovators, creators, and problem-solvers dedicated
+                            to turning ideas into reality and driving meaningful impact.
+                        </p>
                     </div>
+
 
                     <div className="grid max-w-6xl grid-cols-1 px-20 mx-auto mt-12 text-center sm:px-0 sm:grid-cols-2 md:mt-20 gap-x-8 md:grid-cols-4 gap-y-12 lg:gap-x-16 xl:gap-x-20">
                         <div>
-                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-1.png" alt="" />
-                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Jerome Bell</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Co founder, Chairman, Executive Director</p>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Nitish.JPG" alt="Founder" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Nitish Anand</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">CEO & Founder</p>
                         </div>
 
                         <div>
-                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-2.png" alt="" />
-                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Jerome Bell</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Co founder, Chairman, Executive Director</p>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44" src="/assets/HomePage/Ranjan.jpg" alt="Nitish Ranjan" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Nitish Ranjan</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">CFO</p>
                         </div>
 
                         <div>
-                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-3.png" alt="" />
-                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Jerome Bell</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Co founder, Chairman, Executive Director</p>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Raushan.jpg" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Raushan Kumar</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">COO</p>
                         </div>
 
                         <div>
-                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 grayscale filter" src="https://cdn.rareblocks.xyz/collection/clarity/images/team/1/team-member-4.png" alt="" />
-                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Jerome Bell</p>
-                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Co founder, Chairman, Executive Director</p>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Pooja Karkhile</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Senior Manager</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Vishal.jpg" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Vishal Varma</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Business Analyst</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Roshan Gupta</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Mobile App Lead</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Amit.jpg" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Amit Dudhankar</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Software Developer</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Ishwari.jpg" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Ishwari Sonawane</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Software Developer</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Rahul1.JPG" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Rahul Temkar</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Senior Embedded Engineer</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Sachin.JPG" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Sachin Bhagat</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Senior Embedded Engineer</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Rahul2.JPG" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">⁠Rahul Mahato</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Embedded Engineer</p>
+                        </div>
+                        <div>
+                            <img className="object-cover w-32 h-32 mx-auto rounded-full lg:w-44 lg:h-44 " src="/assets/HomePage/Hemant.JPG" alt="" />
+                            <p className="mt-5 text-lg font-bold text-gray-900 sm:text-xl sm:mt-8 font-pj">Hemant Jadhav</p>
+                            <p className="mt-2 text-base font-normal text-gray-600 font-pj">Embedded Engineer</p>
                         </div>
                     </div>
 
